ba1c975e0eaa31d3b0921d9f5d13f7e25e9edef2
Yevgeniy <yevgeniyerot@gmail.com>
2019-03-26 17:04:25 -0400

    Removed test case

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,6 @@
 require_relative '../config/environment'
 
-
 # puts "Yo dog, I heard you like ingredients. You can ingredient while you in ingredient so you can ingredient while you ingredient."
-"Vegan Egg White Substitute Recipe".gsub("Recipe","").rstrip
 
 binding.pry
 0
