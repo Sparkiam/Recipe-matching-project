b2a074358d994be60b82459f35581d0a4bce2c36
Yevgeniy <yevgeniyerot@gmail.com>
2019-03-26 10:29:39 -0400

    Minor changes

--- app/module/tests.rb
+++ app/module/tests.rb
@@ -3,15 +3,25 @@ require 'rest-client'
 require 'json'
 require_relative '../../config/environment.rb'
 
-result = []
+RESULT = []
 
-3.times do |i|
+15.times do |i|
   # binding.pry
   response = RestClient.get("http://www.recipepuppy.com/api/?q=chicken&p=#{i+1}")
   # sleep(1)
   data = JSON.parse(response.body)
   data["results"].each do |food_list|
-    result << food_list
+    RESULT << food_list
+  end
+end
+
+15.times do |i|
+  # binding.pry
+  response = RestClient.get("http://www.recipepuppy.com/api/?q=egg&p=#{i+1}")
+  # sleep(1)
+  data = JSON.parse(response.body)
+  data["results"].each do |food_list|
+    RESULT << food_list
   end
 end
 
@@ -23,7 +33,7 @@ def get_ingredients(arr)
   ingredients.flatten.uniq.sort
 end
 
-my_arr = get_ingredients(result)
+my_arr = get_ingredients(RESULT)
 
 binding.pry
 0
