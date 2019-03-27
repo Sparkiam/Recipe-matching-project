ef8cbec16b0cb716d292731f0c32997390a5e900
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-03-27 17:51:16 -0400

    cli menu

--- bin/run.rb
+++ bin/run.rb
@@ -8,12 +8,16 @@ input = gets.chomp.downcase
 
 current_user = gathering_user_data(input)
 
-kitchen_query(current_user)
-
-puts "So that's your whoooooolllllleeeee kitchen, huh?"
-# sleep(1)
-puts "Are you ready to cook?\n1) 'Yes'\n2) 'Hold on, I've got more food!''\n3) 'I'm out, you are rude!'"
-decision(current_user)
+functions(current_user)
+# 
+#
+#
+# kitchen_query(current_user)
+#
+# puts "So that's your whoooooolllllleeeee kitchen, huh?"
+# # sleep(1)
+# puts "Are you ready to cook?\n1) 'Yes'\n2) 'Hold on, I've got more food!''\n3) 'I'm out, you are rude!'"
+# decision(current_user)
 
 #woudl you like to see the link? would you like to see the ingredients?
 #what rcipe woudl you like to cook
--- lib/cli_methods.rb
+++ lib/cli_methods.rb
@@ -27,7 +27,7 @@ def kitchen_query(user)
   while food != "q"
     food = gets.chomp.downcase
     if food == "h"
-    puts "So, you need help?!\nPress 'i' for an idea of what you have\nOr press 'k' to list what is already in your kitchen!\nPress 'h' to exit this help menu\nPress 'm' to magically add food to your kitchen!\nAnd press 'x' to leave our program!"
+    puts "So, you need help?!\nPress 'i' for an idea of what you have\nOr press 'k' to list what is already in your kitchen!\nPress 'm' to magically add food to your kitchen!\nAnd press 'x' to leave our program!"
     # binding.pry
     elsif food == "i"
       puts Ingredient.get_ingredients_names.sample(10)
@@ -40,7 +40,8 @@ def kitchen_query(user)
     elsif food == "m"
       puts "Cheating is not beneath you I see...."
       sleep(1)
-      puts "Very well, then. "
+      puts "Very well, then. ZOBOOMAFU!!"
+      user.magic
     elsif Ingredient.get_ingredients_names.include?(food)
       user.add_ingredient(food)
       array = ["That's gross", "Wow, me too!", "I just ran out of that!", "My cat likes that too", "We threw one of those away this morning, I hope you didn't get ours...", "#{food.capitalize}! Sounds tasty!!!"]
@@ -51,27 +52,27 @@ def kitchen_query(user)
   end
 end
 
-def decision(current_user)
-  choice = ""
-  while choice != "3"
-    choice = gets.chomp
-    if choice == "1"
-      puts "Let's get started!"
-      cooking(current_user)
-      #should choosing this option break you out of this method?
-    elsif choice == "2"
-      puts "Oh, there's more!"
-      kitchen_query(current_user)
-      puts "Now are you ready to cook?"
-      puts "1) 'Yes!'\n2) 'Hold on, hold on, I've got even MORE food!''\n3)'I'm out, you are very rude!'"
-    elsif choice == "3"
-      break
-    else
-      puts "What!? I didn't catch that"
-    end
-  end
-  puts "Oh okay, TTFN!"
-end
+# def decision(current_user)
+#   choice = ""
+#   while choice != "3"
+#     choice = gets.chomp
+#     if choice == "1"
+#       puts "Let's get started!"
+#       cooking(current_user)
+#       #should choosing this option break you out of this method?
+#     elsif choice == "2"
+#       puts "Oh, there's more!"
+#       kitchen_query(current_user)
+#       puts "Now are you ready to cook?"
+#       puts "1) 'Yes!'\n2) 'Hold on, hold on, I've got even MORE food!''\n3)'I'm out, you are very rude!'"
+#     elsif choice == "3"
+#       break
+#     else
+#       puts "What!? I didn't catch that"
+#     end
+#   end
+#   puts "Oh okay, TTFN!"
+# end
 
 def cooking(current_user)
   puts "So, you have your ingredients, yea?\n\nLet's find out what you can make..."
@@ -83,4 +84,45 @@ def cooking(current_user)
   current_user.list_complete_recipes
   current_user.list_incomplete_recipes
   #it goes back into the decision method. So we need to connect to a new method or break out of that one
+end
+
+def functions(current_user)
+  input = ""
+  while input != "5"
+  puts "\nHow can I help you?"
+  puts "
+  1) Check my Kitchen for Ingredients\n
+  2) See what Recipes I can make\n
+  3) Cook a Recipe\n
+  4) Remove Myself from this confounded Program\n
+  5) Exit the program"
+  input = gets.chomp
+  if input == "1"
+    kitchen_query(current_user)
+  elsif input == "2"
+    cooking(current_user)
+  elsif input == "3"
+    #method
+  elsif input == "4"
+    puts "Oh, are you sure!?\nY/N"
+    confirm = gets.chomp
+    if confirm == "Y"
+      puts "Oh, alright...."
+      sleep(1)
+      puts "I told myself I wouldn't cry.."
+      sleep(1)
+      puts "Very well then"
+      User.remove_user("#{current_user.username}")
+      return "Goodbye"
+    else
+      puts "Oh, you must've hit this by mistake! Sorry about that!"
+    end
+  elsif input == "5" || input == "quit"
+    puts "Loud and clear! Until next time...."
+    break
+  else
+    puts "Nice try, but not nice enough"
+    sleep(1)
+  end
+end
 end
